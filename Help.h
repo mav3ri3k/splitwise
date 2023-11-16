adb1aae8ae873c7604c29bde2cfddd0e4d19c873
Apurva <apurva.jpr@gmail.com>
2023-11-17 01:11:04 +0530

    Use to manage help messages

---
+++ Help.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Help {
+public:
+  string main =
+      "Welcome to expense sharing application\n"
+      "You can add groups for managing expenses\n"
+      "When in doubt, input 'help' for instructions on how to proceed\n";
+
+  string cmdGroups = " - Add: Make new groups\n"
+                     " - View: List groups\n"
+                     " - Exit: Leave the application\n";
+
+  string firstGroup = "Manage group expense here\n";
+  string cmdGroup = " - Add: Make new expense\n"
+                    " - View: List expenses\n"
+                    " - Exit: Leave the application\n";
+};
